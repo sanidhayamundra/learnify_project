f253bd9d5b0c790d5805d58186c5f6b855fc9c81
sanidhayamundra <sanidhaya2005@gmail.com>
2025-07-24 11:12:51 +0530

    fun change

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> ad  test HMR
+          Edit <code>src/App.jsx</code> ad  tst HMR
         </p>
       </div>
       <p className="read-the-docs">
